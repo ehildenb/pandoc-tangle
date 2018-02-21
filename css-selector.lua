d4a64b935e6723141dc72dc19e3ce3b00e09adf7
Everett Hildenbrandt <hildenb2@illinois.edu>
2018-02-20 23:30:35 -0600

    css-selector, test-css-selector: add Lua tokenizer for basic css selectors

---
+++ css-selector.lua
@@ -0,0 +1,27 @@
+tokens = { '%.' , ':not%(' , '%)' , ',' , '%s+' , '[%a%d-]+' }
+
+function tokenize(selector_string)
+    local tokenized_input = {}
+    local found_token = false
+    local match = ""
+
+    while string.len(selector_string) > 0 do
+        --- print("current string: " .. selector_string)
+        found_token = false
+        for _,token in pairs(tokens) do
+            --- print("matching token: " .. token)
+            match = string.match(selector_string, '^' .. token, 1)
+            if match then
+                --- print("token matched!")
+                found_token = true
+                selector_string,_ = string.gsub(selector_string, token, '', 1)
+                table.insert(tokenized_input, match)
+                break
+            end
+        end
+        if not found_token then
+            error("Could not tokenize: " .. selector_string)
+        end
+    end
+    return tokenized_input
+end
