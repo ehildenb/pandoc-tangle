804cfb6dcaae5944fe56fa4e36174bc49bb2242b
Everett Hildenbrandt <hildenb2@illinois.edu>
2018-02-21 12:08:53 -0600

    css-selector: adding group_tokens function

--- css-selector.lua
+++ css-selector.lua
@@ -37,6 +37,34 @@ function tokenize(selector_string)
     return tokenized_input
 end
 
+function group_tokens(tokenized)
+    local grouped_tokens = {}
+    local current_token = 1
+    while tokenized[current_token] do
+        if matches_token(tokenized[current_token], IDSTART) and matches_token(tokenized[current_token + 1], ID) then
+            table.insert(grouped_tokens, { id = tokenized[current_token + 1] })
+            current_token = current_token + 2
+        elseif matches_token(tokenized[current_token], NOTSTART) then
+            local not_group = {}
+            current_token = current_token + 1
+            while not matches_token(tokenized[current_token], NOTEND) do
+                table.insert(not_group, tokenized[current_token])
+                current_token = current_token + 1
+            end
+            current_token = current_token + 1
+            table.insert(grouped_tokens, { notExp = group_tokens(not_group) })
+        elseif matches_token(tokenized[current_token], OR) then
+            table.insert(grouped_tokens, OR)
+            current_token = current_token + 1
+        elseif matches_token(tokenized[current_token], AND) then
+            current_token = current_token + 1
+        else
+            error("Unexpected token: " .. tokenized[current_token], 1)
+        end
+    end
+    return grouped_tokens
+end
+
 function parse_tokens(tokenized)
     return tokenized
 end
