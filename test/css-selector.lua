68815ab72262c7af63f9685b01792521d6face3a
Everett Hildenbrandt <hildenb2@illinois.edu>
2018-02-21 13:14:09 -0600

    test/css-selector: add parse_groups tests

--- test/css-selector.lua
+++ test/css-selector.lua
@@ -63,25 +63,25 @@ tests = {}
 tests[1] = { input  = ".test .input"
            , tokens = { "." , "test" , " " , "." , "input" }
            , groups = { { id = "test" } , { id = "input" } }
-           , ast    = { orExp = { andExp = { { idExp = "test" } , { idExp = "input" } } } }
+           , ast    = { orExp = { { andExp = { { id = "test" } , { id = "input" } } } } }
            }
 
 tests[2] = { input  = ".test , .input"
            , tokens = { "." , "test"  , " , " , "." , "input" }
            , groups = { { id = "test" } , OR , { id = "input" } }
-           , ast    = { orExp = { { andExp = { id = "test" } } , { andExp = { id = "input" } } } }
+           , ast    = { orExp = { { andExp = { { id = "input" } } } , { andExp = { { id = "test" } } } } }
            }
 
 tests[3] = { input  = ".k.transferFrom-then-branch:not(.transferFrom-else-branch)"
            , tokens = { "." , "k" , "." , "transferFrom-then-branch" , ":not(" , "." , "transferFrom-else-branch" , ")" }
            , groups = { { id = "k" } , { id = "transferFrom-then-branch" } , { notExp = { { id = "transferFrom-else-branch" } } } }
-           , ast    = { orExp = { { andExp = { { id = "k" } , { id = "transferFrom-then-branch" } , { notExp = { orExp = { andExp = { id = "transferFrom-else-branch" } } } } } } } }
+           , ast    = { orExp = { { andExp = { { id = "k" } , { id = "transferFrom-then-branch" } , { notExp = { orExp = { { andExp = { { id = "transferFrom-else-branch" } } } } } } } } } }
            }
 
 tests[4] = { input  = ".k .blah :not(.foo.bar)"
            , tokens = { "." , "k" , " " , "." , "blah" , " " , ":not(" , "." , "foo" , "." , "bar" , ")" }
            , groups = { { id = "k" } , { id = "blah" } , { notExp = { { id = "foo" } , { id = "bar" } } } }
-           , ast    = { orExp = { andExp = { { id = "k" } , { id = "blah" } , { notExp = { orExp = { andExp = { { id = "foo" } , { id = "bar" } } } } } } } }
+           , ast    = { orExp = { { andExp = { { id = "k" } , { id = "blah" } , { notExp = { orExp = { { andExp = { { id = "foo" } , { id = "bar" } } } } } } } } } }
            }
 
 --- Run Tests
@@ -105,5 +105,11 @@ for i,_ in pairs(tests) do
     local grouped = group_tokens(tests[i]["tokens"])
     if not deepcompare(grouped, tests[i]["groups"]) then
         test_error(tests[i]["groups"], grouped)
+    end
+
+    print("testing group parser...")
+    local parsed = parse_groups(tests[i]["groups"])
+    if not deepcompare(parsed, tests[i]["ast"]) then
+        test_error(tests[i]["ast"], parsed)
     end
 end
